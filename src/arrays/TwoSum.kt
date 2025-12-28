b08aefbc5c6a0b39bfa7a466fa3a34aad9717f86
Chaudhary Anil <chaudharyanil5656@gmail.com>
2025-12-28 20:56:49 +0545

    Add TwoSum solution in Kotlin

--- src/arrays/TwoSum.kt
+++ src/arrays/TwoSum.kt
@@ -1,2 +1,31 @@
 package arrays
 
+/*
+Problem:
+Given an array of integers and a target, return indices of
+two numbers such that they add up to target.
+
+Approach:
+Use a map to store number → index, then check if complement exists.
+
+*/
+
+class TwoSum {
+    fun twoSum(num: IntArray, target: Int): IntArray {
+        val map = mutableMapOf<Int, Int>()
+        for (i in num.indices) {
+            val need = target - num[i]
+            if (map.containsKey(need)) {
+                return intArrayOf(map[need]!!, i)
+            }
+            map[num[i]] = i
+        }
+        return intArrayOf()
+    }
+}
+
+fun main() {
+    val ts = TwoSum()
+    val result = ts.twoSum(num = intArrayOf(2,8,7,9,11) , 9)
+    println(result.contentToString())
+}
